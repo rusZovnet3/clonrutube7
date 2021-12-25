cd60c73e76da7358d6577d49e78ddebdd7b71134
rusZovnet3 <cferls_ru@hotmail.com>
2021-12-25 09:48:27 -0400

    Se realizo la parte del menu con la lista de cuadros, con el respectivo responsive del archivo index.html, ademas se creo un nuevo archivo play-video.html la cual se hizo la copia del navbar, implementando el sidebar derecho que incluye la lista de cuadros de videos, tambien se implemento el contenido de visualizacion del video con su respectivo casi similar al youtube v1.1.3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var menu_icono = document.querySelector(".menu-icono");
 var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
 
 menu_icono.onclick = function(){
 	sidebar.classList.toggle("small-sidebar");
+	container.classList.toggle("large-container");
 }
